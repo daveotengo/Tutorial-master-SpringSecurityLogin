c91822ee659d96793ef04b9e4745bf426e39eb7c
daveotengo <daveotengo@gmail.com>
2022-01-12 18:35:11 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tutorial-SpringSecurityLogin
+
+1. mvn clean
+2. mvn clean install
+3. Go to the target folder
+4. java -jar demo-0.0.1-SNAPSHOT.jar
+
+- http://localhost:9080/registration
+- http://localhost:9080/login
+
---
+++ src/main/java/com/example/model/Role.java
@@ -0,0 +1,45 @@
+package com.example.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+
+@Entity
+@Table(name = "role")
+public class Role implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8987751528997143706L;
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="role_id")
+	private int id;
+	@Column(name="role")
+	private String role;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
